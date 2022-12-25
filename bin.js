fa641615bc57eed729608788d5ecffe98be97f7f
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-24 20:27:14 -0600

    comments

--- bin.js
+++ bin.js
@@ -2,5 +2,7 @@
 
 console.log('bin.js > Hello, world!');
 
-console.log(`__dirname=${__dirname}`);
-console.log(`process.cwd()=${process.cwd()}`);
+// __dirname returns where npx cache the installation
+console.log(`__dirname=${__dirname}`); // __dirname=/Users/kuang/.npm/_npx/06be232737db8544/node_modules/@zjkuang/i18next-wizard
+// process.cwd() returns the folder where the npx command is running
+console.log(`process.cwd()=${process.cwd()}`); // process.cwd()=/Users/kuang
