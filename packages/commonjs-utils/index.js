fbe56090c099380a6c593146341f79c15ef9ce9f
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-26 09:43:24 -0600

    testing

--- packages/commonjs-utils/index.js
+++ packages/commonjs-utils/index.js
@@ -1,5 +1,7 @@
 const appLocation = () => {
-  const dir = __dirname.replace('/node_modules/@zjkuang/commonjs-utils', '');
+  const dir = __dirname
+  .replace('/node_modules/@zjkuang/commonjs-utils', '') // local `npx .`
+  .replace('/packages/commonjs-utils', ''); // remote `npx <app-package>`
   return dir;
 }
 
