25c34630bbef06c6e5d3775a9fd19192da82f63e
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-26 12:52:58 -0600

    testing

--- bin.js
+++ bin.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 import {py} from './src/python-shell/index.js';
-import {appLocation, get_argv} from '@zjkuang/commonjs-utils';
+import yargs from 'yargs/yargs';
+import {hideBin} from 'yargs/helpers';
+import {appLocation} from '@zjkuang/commonjs-utils';
 
 console.log('bin.js > Hello, world!');
 
+const argv = yargs(hideBin(process.argv)).argv;
+console.log(`yargs > ${JSON.stringify(argv)}`);
+
 console.log(`utils > appLocation: ${appLocation()}`);
-console.log(`utils > argv: ${JSON.stringify(get_argv())}`);
 
 py();
--- packages/commonjs-utils/index.js
+++ packages/commonjs-utils/index.js
@@ -5,14 +5,6 @@ const appLocation = () => {
   return dir;
 }
 
-const get_argv = () => {
-  const yargs = require('yargs/yargs');
-  const { hideBin } = require('yargs/helpers');
-  const _argv = yargs(hideBin(process.argv)).argv;
-  return _argv;
-}
-
 module.exports = {
   appLocation,
-  get_argv,
 }
