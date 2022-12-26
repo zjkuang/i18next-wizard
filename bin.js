580981d3a823159a42ec201161c3f88a30aebfaf
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-26 11:26:58 -0600

    testing

--- bin.js
+++ bin.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 import {py} from './src/python-shell/index.js';
-import {appLocation} from '@zjkuang/commonjs-utils';
+import {appLocation, get_argv} from '@zjkuang/commonjs-utils';
 
 console.log('bin.js > Hello, world!');
 
 console.log(`utils > appLocation: ${appLocation()}`);
+console.log(`utils > argv: ${JSON.stringify(get_argv())}`);
 
 py();
